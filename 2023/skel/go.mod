63ce03292bb10568759a410fa046f8df868c62a2
Loup Menjoulet <lmenjoulet@laposte.net>
2023-12-01 23:14:11 +0700

    improved folder structure

--- 2023/skel/go.mod
+++
@@ -1,3 +0,0 @@
-module skel
-
-go 1.21.4
---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
+
+if [[ month -eq 12 ]] && [[ day -lt 25 ]]; then
+  if [ ! -d ./$year/day_$day ]; then  
+    cp -r skel ./$year/day_$day
+    go mod init day_$day
+  fi
+  cd $year/day_$day
+  $EDITOR input.txt
+else
+  echo "Time isn't right"
+fi
+
