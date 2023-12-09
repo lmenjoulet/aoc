739eae1f57d4cad694e0f621f6e4170529efdfa0
Loup Menjoulet <lmenjoulet@laposte.net>
2023-12-09 14:18:45 +0700

    finished day 4 part 2

--- 2023/day_04/2.py
+++ 2023/day_04/2.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 
+cards = [] 
+
 with open('./input.txt') as file:
     lines = file.readlines()
+    sum = 0
     for idx in range(len(lines)):
         lines[idx] = lines[idx].rstrip("\n")
+        win, got = lines[idx].split(":")[1].split("|")
+        win = list(filter(lambda x: x != "", win.split(" ")))
+        got = list(filter(lambda x: x != "", got.split(" ")))
+        cards.append(
+            list(
+                range(
+                    idx + 1, 
+                    idx + 1 +len(set(win).intersection(set(got)))
+                )
+            )
+        )
+
+def sumcards(cardlist):
+    sum = len(cardlist)
+    for got in cardlist:
+        sum += sumcards(cards[got])
+    return sum
+
+sum = len(cards)
+for card in cards:
+    sum += sumcards(card)
+print(sum)
+
 
