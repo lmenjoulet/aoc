28256ad0d2adcb21750100ef365a1d7d31625d80
Loup Menjoulet <lmenjoulet@laposte.net>
2024-11-29 16:22:41 +0700

    2023: solved day 6 part 2

--- 2023/day_06/2.jl
+++ 2023/day_06/2.jl
@@ -2,8 +2,39 @@
 
 lines = []
 
-open("./input.txt") do file
+open("input.txt") do file
   for line in eachline(file)
-    push!(lines,strip(line,"\n"))
+    push!(lines,line)
   end
 end
+
+fix_kerning = map(x -> replace(x, r"(\d) +(\d)" => s"\1\2"), lines)
+numbers = map(x -> [parse(Int, m.match) for m in eachmatch(r"\d+", x)] , fix_kerning)
+races = reshape(vcat(numbers...),(:,2))
+
+possible_per_race = [ ]
+for race in eachrow(races)
+  total_time = race[1]
+  best_distance = race[2]
+
+  discriminant = total_time^2 - 4*best_distance
+  x_1 = ((-total_time + discriminant^0.5)/-2)
+  x_2 = ((-total_time - discriminant^0.5)/-2)
+  
+  if ceil(x_1) == x_1 
+    x_1 = ceil(x_1)+1
+  else
+    x_1 = ceil(x_1)
+  end
+
+  if floor(x_2) == x_2 
+    x_2 = floor(x_2)-1
+  else
+    x_2 = floor(x_2)
+  end
+  
+  push!(possible_per_race, x_2 - x_1 + 1)
+end
+
+println(prod(possible_per_race))
+
