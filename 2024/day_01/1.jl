344a80f3d2e363f68c1189af4aa6e2934fe0456e
Loup Menjoulet <lmenjoulet@laposte.net>
2024-12-02 11:48:46 +0700

    solved: 2024 day 1 part 1

--- 2024/day_01/1.jl
+++ 2024/day_01/1.jl
@@ -7,3 +7,13 @@ open("./input.txt") do file
     push!(lines,line)
   end
 end
+
+cols = map(x -> sort(x),
+  eachrow(
+    reshape(
+      vcat(map(
+        line -> map(n -> parse(Int, n), split(line)), 
+        lines)...),
+      (2,:))))
+
+println(sum([abs(pair[1]-pair[2]) for pair in zip(cols[1], cols[2])]))
