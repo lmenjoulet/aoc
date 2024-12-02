21deb95fedcab0e0b2d6dec4bd9138c9bcdfd053
Loup Menjoulet <lmenjoulet@laposte.net>
2024-12-02 12:07:21 +0700

    solved: 2024 day 2 part 2

--- 2024/day_01/2.jl
+++ 2024/day_01/2.jl
@@ -7,3 +7,18 @@ open("./input.txt") do file
     push!(lines,line)
   end
 end
+
+cols = eachrow(
+    reshape(
+      vcat(map(
+        line -> map(n -> parse(Int, n), split(line)), 
+        lines)...),
+      (2,:)))
+
+ammount = Dict{Int,Int}()
+
+map(x -> haskey(ammount,x) ? ammount[x] += 1 : ammount[x] = 1, cols[2])
+
+total = 0
+map(x -> if (haskey(ammount,x)) global total += ammount[x]*x end, cols[1])
+println(total)
