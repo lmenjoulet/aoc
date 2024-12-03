e62206e77e1c8f2dc9056a089e96da5ab9e3b53a
Loup Menjoulet <lmenjoulet@laposte.net>
2024-12-03 15:25:48 +0700

    skel: updated template

--- skel/2.jl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env julia 
-
-lines = []
-
-open("./input.txt") do file
-  for line in eachline(file)
-    push!(lines,line)
-  end
-end
---
+++ skel/silver.jl
@@ -0,0 +1,4 @@
+#!/usr/bin/env julia 
+
+text = read("input.txt", String)
+# lines = split(text,"\n")
