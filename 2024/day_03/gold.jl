0a8a9c8a20f071554cf8e15637b277a96267def0
Loup Menjoulet <lmenjoulet@laposte.net>
2024-12-04 10:03:58 +0700

    cleaned: 2024 day 3 both parts

--- 2024/day_03/gold.jl
+++ 2024/day_03/gold.jl
@@ -1,14 +1,6 @@
 #!/usr/bin/env julia 
 
-lines = []
-
-open("./input.txt") do file
-  for line in eachline(file)
-    push!(lines, line)
-  end
-end
-
-text = join(lines)
+text = read("input.txt", String)
 
 do_enabled = join(split(text, r"don't\(\).*?do\(\)"), "a") # insert trash character to avoid potential "mul(1don't()do(),1)"
 clean_pairs = [map(str -> parse(Int, str), match.captures) for match in eachmatch(r"mul\((\d+),(\d+)\)", do_enabled)]
